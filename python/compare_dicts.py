5ab2bb4ba86585c4ab828c2f88516345074c1d35
sjai013 <sjai013.ece@gmail.com>
2015-08-21 12:56:40 +1200

    Code updated.  Does more.

--- python/compare_dicts.py
+++ python/compare_dicts.py
@@ -1,17 +1,44 @@
 __author__ = 'sjai013'
 
+# Read ALD lexicon (in a transformed SAMPA format)
+
 f = open("../ald/lexicon.txt");
-a = f.readlines();
+temp = f.readlines();
 
 ald = dict()
-for line in a:
-    ald[line.split()[0]] = line.split()[1]
+for line in temp:
+    ald[line.split("\t")[0]] = line.split("\t")[1].replace("\n","")
 
+# Read MAUS dictionary (in SAMPA format)
 
 f = open("../alveo/all.txt")
-a = f.readlines();
+temp = f.readlines();
 
 alveo = dict()
-for line in a:
+for line in temp:
     alveo[line.split(",")[0]] = line.split(",")[1].replace("\n","")
 
+
+# Transform the ALD pronunciation to SAMPA - need to define a 1:1 transformation
+# Not all phones are one character long - some are two.  So, need to begin search with two-character long phones
+
+#####################################################################
+###################### Transform ALD to SAMPA #######################
+#####################################################################
+# Now we can compare the two dictionaries, and find:
+# (1) words that are unique to each dictionary,
+# (2) words that have differing pronunciations in the dictionary
+
+
+#####################################################################
+##### (1) Find unique words (i.e. words only in one dictionary) #####
+#####################################################################
+unique_words = list(set(ald.keys()) ^ set(alveo.keys()))
+
+# Also find words that are in both dictionaries (so we can compute the
+# Modified SAMPA --> SAMPA rules)
+common_words = list(set(ald.keys()) & set(alveo.keys()))
+
+#####################################################################
+########### (2) Find words with differing pronunciations ############
+#####################################################################
